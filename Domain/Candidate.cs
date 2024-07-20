f96611faaab9b250151c9d05bc782dd431e351a4
iustin <iustindeaconu@gmail.com>
2024-07-20 11:18:12 +0300

    Add project files.

---
+++ Domain/Candidate.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain;
+
+public class Candidate
+{
+    [Key]
+    public long Id { get; set; }
+
+    [Required]
+    public string FirstName { get; set; }
+
+    [Required]
+    public string LastName { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    [Required]
+    public string Email { get; set; }
+
+    public string LinkedInProfile { get; set; }
+
+    public string GitHubProfile { get; set; }
+
+    public string Comment { get; set; }
+}
