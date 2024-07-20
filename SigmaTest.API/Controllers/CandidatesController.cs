6633bcf1311694a742bfd94d726513f55d602b6c
iustin <iustindeaconu@gmail.com>
2024-07-20 13:32:16 +0300

    Added endpoint to add/ update candidate

---
+++ SigmaTest.API/Controllers/CandidatesController.cs
@@ -0,0 +1,36 @@
+﻿using ApplicationCore.Candidates.Commands;
+using Domain;
+using Domain.Models;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SigmaTest.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CandidatesController : ControllerBase
+{
+	private readonly IMediator _mediator;
+	private readonly IValidator<UpsertCandidateCmd> _validator;
+
+    public CandidatesController(IMediator mediator, IValidator<UpsertCandidateCmd> validator)
+    {
+        _mediator = mediator;
+        _validator = validator;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Candidate>> UpsertCandidate([FromBody] CreateCandidateModel candidate, CancellationToken cancellationToken)
+    {
+		var cmd = new UpsertCandidateCmd { model = candidate };
+		var validation = _validator.Validate(cmd);
+		if (!validation.IsValid)
+		{
+			return BadRequest(validation.ToString());
+		}
+
+        var result = await _mediator.Send(cmd, cancellationToken);
+		return Ok(result);
+    }
+}
