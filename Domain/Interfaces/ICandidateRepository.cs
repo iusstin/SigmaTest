94cee75b58b14d395c33da9aa33d846d63ddb430
iustin <iustindeaconu@gmail.com>
2024-07-20 12:15:27 +0300

    Add repository for db actions on candidates

---
+++ Domain/Interfaces/ICandidateRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Interfaces;
+
+public interface ICandidateRepository
+{
+    Task<IEnumerable<Candidate>> GetAll(CancellationToken cancellationToken);
+    Task<Candidate?> Get(long id);
+    Task<long?> AddCandidate(Candidate candidate, CancellationToken cancellationToken);
+    Task<Candidate> UpdateCandidate(Candidate candidate, CancellationToken cancellationToken);
+}
---
+++ Infrastructure/CandidateRepository.cs
@@ -0,0 +1,70 @@
+﻿using Domain;
+using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Infrastructure;
+
+public class CandidateRepository : ICandidateRepository
+{
+    private readonly IAppDbContext _dbContext;
+
+    public CandidateRepository(IAppDbContext dbContext) 
+        => _dbContext = dbContext;
+
+    public async Task<long?> AddCandidate(Candidate candidate, CancellationToken cancellationToken)
+    {
+        await _dbContext.Candidates.AddAsync(candidate, cancellationToken);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+        return candidate.Id;
+    }
+
+    public async Task<Candidate?> Get(long id)
+    {
+        var candidate = await _dbContext.Candidates.FirstOrDefaultAsync(x => x.Id == id);
+        return candidate;
+    }
+
+    public async Task<IEnumerable<Candidate>> GetAll(CancellationToken cancellationToken)
+    {
+        var candidates = QueryCandidates();
+        return await candidates.ToListAsync(cancellationToken);
+    }
+
+    public async Task<Candidate> UpdateCandidate(Candidate candidate, CancellationToken cancellationToken)
+    {
+        _dbContext.Candidates.Update(candidate);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+        return candidate;
+    }
+
+    private IQueryable<Candidate> QueryCandidates(
+        Expression<Func<Candidate, bool>>? filter = null,
+        Func<IQueryable<Candidate>, IOrderedQueryable<Candidate>>? orderBy = null,
+        bool trackChanges = false,
+        bool ignoreQueryFilters = false)
+    {
+        var candidates = _dbContext.Candidates.AsQueryable();
+        if (!trackChanges)
+        {
+            candidates = candidates.AsNoTracking();
+        }
+
+        if (ignoreQueryFilters)
+        {
+            candidates = candidates.IgnoreQueryFilters();
+        }
+
+        if (filter is not null)
+        {
+            candidates = candidates.Where(filter);
+        }
+
+        if (orderBy is not null)
+        {
+            candidates = orderBy(candidates);
+        }
+
+        return candidates;
+    }
+}
