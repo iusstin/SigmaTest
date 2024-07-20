f461fc12aef438985ca92589b9eb38a40eafa646
iustin <iustindeaconu@gmail.com>
2024-07-20 13:30:51 +0300

    Validator for required fields for candidates

---
+++ ApplicationCore/UpsertCandidateValidator.cs
@@ -0,0 +1,15 @@
+﻿using ApplicationCore.Candidates.Commands;
+using FluentValidation;
+
+namespace ApplicationCore;
+
+public class UpsertCandidateValidator : AbstractValidator<UpsertCandidateCmd>
+{
+    public UpsertCandidateValidator()
+    {
+        RuleFor(c => c.model.Email).NotEmpty().WithMessage("Email cannot be empty");
+        RuleFor(c => c.model.FirstName).NotEmpty().WithMessage("First name cannot be empty");
+        RuleFor(c => c.model.LastName).NotEmpty().WithMessage("Last name cannot be empty");
+        RuleFor(c => c.model.Comment).NotEmpty().WithMessage("Comment cannot be empty");
+    }
+}
